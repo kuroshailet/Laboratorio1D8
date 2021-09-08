eca0df0781e27e34d7a47e5961f3a9614116603c
Dayron Zapata <dayron6000@hotmail.com>
2021-09-07 21:35:30 -0500

    Se realizo el problema 1.

---
+++ Laboratorio1D8/Problema1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Problema1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalcularSalario.Click
+        Dim salarioB As Double
+
+        If (Val(txtHorasTra.Text) > 0 And Val(txtTarifaHor.Text) > 0) Then
+            salarioB = Val(txtHorasTra.Text) * Val(txtTarifaHor.Text)
+            txtSalarioBruto.Text = salarioB
+            txtSalarioNeto.Text = salarioB - ((salarioB * 12) / 100)
+        Else
+            MsgBox("Valores invalidos.")
+        End If
+
+    End Sub
+End Class
