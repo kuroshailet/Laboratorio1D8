d4de7a821078ea0c101541d34bd5b8de8705f4dc
Dayron Zapata <dayron6000@hotmail.com>
2021-09-07 22:21:25 -0500

    Haciendo cambios pero tube que parar

---
+++ Laboratorio1D8/Problema5.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Problema5
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtOferta1 = New System.Windows.Forms.TextBox()
+        Me.txtOferta2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtOferta3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtOferta6 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtOferta5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtOferta4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtOferta9 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtOferta8 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtOferta7 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtOferta10 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnCalcular)
+        Me.GroupBox1.Controls.Add(Me.txtOferta10)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.txtOferta9)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.txtOferta8)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.txtOferta7)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.txtOferta6)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtOferta5)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtOferta4)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txtOferta3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtOferta2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtOferta1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 21)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(224, 410)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(17, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Oferta 1"
+        '
+        'txtOferta1
+        '
+        Me.txtOferta1.Location = New System.Drawing.Point(93, 40)
+        Me.txtOferta1.Name = "txtOferta1"
+        Me.txtOferta1.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta1.TabIndex = 1
+        '
+        'txtOferta2
+        '
+        Me.txtOferta2.Location = New System.Drawing.Point(93, 68)
+        Me.txtOferta2.Name = "txtOferta2"
+        Me.txtOferta2.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(17, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 22)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Oferta 2"
+        '
+        'txtOferta3
+        '
+        Me.txtOferta3.Location = New System.Drawing.Point(93, 96)
+        Me.txtOferta3.Name = "txtOferta3"
+        Me.txtOferta3.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta3.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(17, 99)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 22)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Oferta 3"
+        '
+        'txtOferta6
+        '
+        Me.txtOferta6.Location = New System.Drawing.Point(93, 180)
+        Me.txtOferta6.Name = "txtOferta6"
+        Me.txtOferta6.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta6.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(17, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 22)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Oferta 6"
+        '
+        'txtOferta5
+        '
+        Me.txtOferta5.Location = New System.Drawing.Point(93, 152)
+        Me.txtOferta5.Name = "txtOferta5"
+        Me.txtOferta5.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta5.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(17, 155)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(70, 22)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Oferta 5"
+        '
+        'txtOferta4
+        '
+        Me.txtOferta4.Location = New System.Drawing.Point(93, 124)
+        Me.txtOferta4.Name = "txtOferta4"
+        Me.txtOferta4.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta4.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(17, 127)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 22)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Oferta 4"
+        '
+        'txtOferta9
+        '
+        Me.txtOferta9.Location = New System.Drawing.Point(93, 264)
+        Me.txtOferta9.Name = "txtOferta9"
+        Me.txtOferta9.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta9.TabIndex = 17
+        '
+        'Label7
+        '
+        Me.Label7.Location = New System.Drawing.Point(17, 267)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(70, 22)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Oferta 9"
+        '
+        'txtOferta8
+        '
+        Me.txtOferta8.Location = New System.Drawing.Point(93, 236)
+        Me.txtOferta8.Name = "txtOferta8"
+        Me.txtOferta8.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta8.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.Location = New System.Drawing.Point(17, 239)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(70, 22)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Oferta 8"
+        '
+        'txtOferta7
+        '
+        Me.txtOferta7.Location = New System.Drawing.Point(93, 208)
+        Me.txtOferta7.Name = "txtOferta7"
+        Me.txtOferta7.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta7.TabIndex = 13
+        '
+        'Label9
+        '
+        Me.Label9.Location = New System.Drawing.Point(17, 211)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(70, 22)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Oferta 7"
+        '
+        'txtOferta10
+        '
+        Me.txtOferta10.Location = New System.Drawing.Point(93, 292)
+        Me.txtOferta10.Name = "txtOferta10"
+        Me.txtOferta10.Size = New System.Drawing.Size(100, 22)
+        Me.txtOferta10.TabIndex = 19
+        '
+        'Label10
+        '
+        Me.Label10.Location = New System.Drawing.Point(17, 295)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(70, 22)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Oferta 10"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(72, 364)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 20
+        Me.btnCalcular.Text = "Button1"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(242, 21)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(546, 410)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'Problema5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Problema5"
+        Me.Text = "Problema5"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtOferta10 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtOferta9 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtOferta8 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtOferta7 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtOferta6 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtOferta5 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtOferta4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtOferta3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtOferta2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtOferta1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ Laboratorio1D8/Problema5.vb
@@ -0,0 +1,5 @@
+﻿Public Class Problema5
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Me.Controls(String.Format("txtOferta2")).Text = 2
+    End Sub
+End Class
